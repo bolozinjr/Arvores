919b1a869a904ddb4e59464ea086df6584cea1b9
Marcos Vinicios <168040265+bolozinjr@users.noreply.github.com>
2025-05-29 20:24:48 -0300

    buscaEmNivel

--- src/No.java
+++ src/No.java
@@ -1,10 +1,12 @@
 public class No {
     String valor;
     No esquerda, direita;
+    int fatorBalanceamento;
 
     public No(String valor) {
         this.valor = valor;
         esquerda = direita = null;
+        this.fatorBalanceamento = 0;
     }
 }
 
